d54194481f1b9bb06a613e9792cd69976356a280
nadiaehwan <78330239+nadiaehwan@users.noreply.github.com>
2021-02-17 16:16:44 +0800

    Update and rename keys.js to config/keys.js

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MongoURI: 
+    'mongodb+srv://<username>:<password>@<clustername>.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
--- keys.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MongoURI: 
-    'mongodb+srv://sitihajar:Babanyonya96@freecluster.j8uru.mongodb.net/JohnLifeWheel?retryWrites=true&w=majority'
-}
